1b361db8262db07ca84dbca71c44a96b3838a9cd
moo-light <binhnguyenthanh19242yahoo@gmail.com>
2022-11-15 09:56:33 +0700

    Prefect commit

--- BusinessObject/Models/FstoreDbContext.cs
+++ BusinessObject/Models/FstoreDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace BusinessObject.Models;
@@ -27,7 +25,6 @@ public partial class FstoreDbContext : DbContext
     public virtual DbSet<Product> Products { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer(GetConnectionString());
     private string GetConnectionString()
     {
