3e46bf42ff5c3ed7eadd860ce1efd02fa5509183
moo-light <binhnguyenthanh19242yahoo@gmail.com>
2022-11-17 21:50:52 +0700

    CartObject

---
+++ BusinessObject/CartObject/CartObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.CartObject
+{
+    public class CartObject
+    {
+        public Dictionary<int, int> Cart { get; set; }
+        void AddToCart(int productID) { }
+        void RemoveFromCart(int productID) { }
+
+    }
+}
