85f6db3b3b5afae193d1f1e936f75b5ac1f3084d
hvpexe <phuhuynh923@gmail.com>
2022-11-17 15:18:02 +0700

    product manage

--- SalesWinApp/frmProduct.cs
+++ SalesWinApp/frmProduct.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessObject.Models;
 using DataAccess.Repository;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -27,9 +28,8 @@ namespace SalesWinApp
         {
             LoadProduct();
         }
-        private void LoadProduct(string search = null)
+        private void LoadProduct()
         {
-            //1. lay toan bo account tu db
             var products = _repository.GetAllProduct().Select(
                 p => new {
                     ProductId = p.ProductId,
@@ -41,21 +41,120 @@ namespace SalesWinApp
                 }
                 );
 
-            if (search != null)
+            _source = new BindingSource();
+            _source.DataSource = products;
+            dgvProduct.DataSource = _source;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var products = _repository.GetAllProduct().Select(
+                p => new {
+                    ProductId = p.ProductId,
+                    ProductName = p.ProductName,
+                    Weight = p.Weight,
+                    UnitPrice = p.UnitPrice,
+                    UnitsInStock = p.UnitsInStock,
+                    CategoryName = p.Category.CategoryName
+                }
+                );
+
+            var minPriceText = txtMinPrice.Text;
+            var maxPriceText = txtMaxPrice.Text;
+            if (!minPriceText.IsNullOrEmpty() && !maxPriceText.IsNullOrEmpty())
             {
-                products = products.Where(x => x.ProductName.ToUpper().Contains(search.ToUpper().Trim()));
+                try
+                {
+                    int minPrice = Int32.Parse(minPriceText);
+                    int maxPrice = Int32.Parse(maxPriceText);
+
+                    if (minPrice > maxPrice)
+                    {
+                        throw new InvalidDataException("Min price must be smaller than max price.");
+                    }
+                    products = products.Where(x => x.UnitPrice >= minPrice && x.UnitPrice <= maxPrice);
+
+                }
+                catch (InvalidDataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("You must type number!");
+                }
+            }
+
+            var minStockText = txtMinStock.Text;
+            var maxStockText = txtMaxStock.Text;
+            if (!minStockText.IsNullOrEmpty() && !maxStockText.IsNullOrEmpty())
+            {
+                try
+                {
+                    int minStock = Int32.Parse(minStockText);
+                    int maxStock = Int32.Parse(maxStockText);
+
+                    if (minStock > maxStock)
+                    {
+                        throw new InvalidDataException("Min stock must be smaller than max stock.");
+                    }
+                    products = products.Where(x => x.UnitsInStock >= minStock && x.UnitsInStock <= maxStock);
+
+                }
+                catch (InvalidDataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("You must type number!");
+                }
+            }
+
+            var search = txtSearch.Text.ToUpper();
+            var searchBy = cboSearchBy.Text;
+            if (!search.IsNullOrEmpty())
+            {
+                if (searchBy.Equals("ID"))
+                {
+                    products = products.Where(x => x.ProductId.ToString().Equals(search));
+                }
+                else if (searchBy.Equals("ProductName"))
+                {
+                    products = products.Where(x => x.ProductName.ToUpper().Contains(search.ToUpper().Trim()));
+                }
             }
 
             _source = new BindingSource();
             _source.DataSource = products;
-            //2. add to dgv
             dgvProduct.DataSource = _source;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
 
         }
 
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you want to delete this item?", "Confirm delete", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                var account = _repository.GetAllProduct().ToList()[dgvProduct.CurrentRow.Index];
+                _repository.Delete(account);
+                LoadProduct();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
-
-    
-
 
 }
