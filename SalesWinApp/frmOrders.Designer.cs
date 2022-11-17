f1fe9675d13bb985bad506e4bf4cdb923b6735d0
doctorpain20 <84951795+mrnobody1273@users.noreply.github.com>
2022-11-17 13:42:01 +0700

    Update Order Detail

--- SalesWinApp/frmOrders.Designer.cs
+++ SalesWinApp/frmOrders.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(645, 394);
+            this.btnClose.Location = new System.Drawing.Point(761, 514);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(86, 31);
             this.btnClose.TabIndex = 0;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -47,29 +48,34 @@
             // 
             this.lbViewOrderDetail.AutoSize = true;
             this.lbViewOrderDetail.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbViewOrderDetail.Location = new System.Drawing.Point(76, 56);
+            this.lbViewOrderDetail.Location = new System.Drawing.Point(87, 75);
             this.lbViewOrderDetail.Name = "lbViewOrderDetail";
-            this.lbViewOrderDetail.Size = new System.Drawing.Size(150, 25);
+            this.lbViewOrderDetail.Size = new System.Drawing.Size(185, 30);
             this.lbViewOrderDetail.TabIndex = 1;
             this.lbViewOrderDetail.Text = "View Order Detail";
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 120);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 160);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(785, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(897, 200);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // frmOrders
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lbViewOrderDetail);
             this.Controls.Add(this.btnClose);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmOrders";
             this.Text = "frmOrders";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- SalesWinApp/frmOrders.cs
+++ SalesWinApp/frmOrders.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace SalesWinApp
 {
     public partial class frmOrders : Form
     {
+        string connectionString = @"Data Sourc=(local); Initial Catalog = FStoreDB; Integrated Security=True";
         public frmOrders()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM OrderDetial", sqlCon);
+                DataTable dtbl = new DataTable();
+                sqlDa.Fill(dtbl);
+            }
         }
     }
 }
