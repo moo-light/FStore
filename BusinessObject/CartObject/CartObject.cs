f1cf33d7152da6332ab0e46b7110d71674c17f1c
moo-light <binhnguyenthanh19242yahoo@gmail.com>
2022-11-17 21:56:06 +0700

    cartObject

--- BusinessObject/CartObject/CartObject.cs
+++ BusinessObject/CartObject/CartObject.cs
@@ -9,8 +9,26 @@ namespace BusinessObject.CartObject
     public class CartObject
     {
         public Dictionary<int, int> Cart { get; set; }
-        void AddToCart(int productID) { }
-        void RemoveFromCart(int productID) { }
+        void AddToCart(int productID) {
+            if(Cart.ContainsKey(productID)){
+                Cart[productID] += 1;
+            }
+            else
+            {
+                Cart.Add(productID, 1);
+            }
+        }
+        void RemoveFromCart(int productID)
+        {
+            if (Cart.ContainsKey(productID))
+            {
+                Cart[productID] -= 1;
+                if (Cart[productID]==0)
+                {
+                    Cart.Remove(productID);
+                }
+            }
+        }
 
     }
 }
