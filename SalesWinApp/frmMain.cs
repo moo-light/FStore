567a938a1cbf1f48f7e02ffe4f36f838465b7c3c
Nang13 <99975060+Nang13@users.noreply.github.com>
2022-11-17 18:15:22 +0700

    fix bug 8

--- SalesWinApp/frmMain.cs
+++ SalesWinApp/frmMain.cs
@@ -160,5 +160,12 @@ namespace SalesWinApp
             childForm.Show();
 
         }
+
+        private void viewOrderHistoryToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            var childForm = new frmOrderHistory();
+            childForm.MdiParent = this;
+            childForm.Show();
+        }
     }
 }
